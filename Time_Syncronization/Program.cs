cff4e82befe12140b58bd47ec5198f3635151a68
ParkEuiKyu <euikyu0423@gmail.com>
2020-12-02 11:19:29 +0900

    초기 커밋.

---
+++ Time_Syncronization/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Syncronization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
